31810c81173648c935aee43dc2595db8e867e914
kmivehchi-xRoird <kammivehchi@gmail.com>
2023-03-13 15:03:22 -0700

    remove borders from hero

--- pages/index.js
+++ pages/index.js
@@ -11,58 +11,47 @@ import HomeGrid from '../components/HomeGrid'
 const inter = Inter({ subsets: ['latin'] })
 
 const HeroContainer = styled.section`
-position: relative;
-text-align: center;
-z-index:-1;
-max-width:${({ theme }) => theme.breakpoints.xl};
-margin:0 auto;
-div{
-  background-color:rgb(0,0,0,.3);
-  width:100%;
-height:min(50vh,20em);
+  position: relative;
+  text-align: center;
+  z-index:-1;
+  max-width:${({ theme }) => theme.breakpoints.xl};
+  margin:0 auto;
 
+    div{
+      background-color:rgb(0,0,0,.3);
+      width:100%;
+      height:min(50vh,20em);
+      position:absolute;
+      border-radius: inherit;
+    }
+    video{
+      height:min(50vh,20em);
+      width:100%;
+      object-fit:cover;
+      border:.5em solid ${({ theme }) => theme.color.accent};
+      border-left:0;
+      border-right:0;
+      border-bottom:0;
+    }
 
- position:absolute;
- border-radius: inherit;
-}
-video{
-height:min(50vh,20em);
-width:100%;
-object-fit:cover;
-  border:.5em solid ${({ theme }) => theme.color.accent};
-  border-left:0;
-  border-right:0;
-  
-}
+    h1 {
+      color:${({ theme }) => theme.color.fontLight};
+      text-shadow:2px 2px .25em  ${({ theme }) => theme.color.accent2};
+      font-weight:bold;
+      font-size:3em;
+      width:100%;
+    }
 
-h1 {
-  color:${({ theme }) => theme.color.fontLight};
-  // filter: drop-shadow(0 0 em 3em rgb(0,0,0));
-  filter: drop-shadow(2px 2px .25em  ${({ theme }) => theme.color.accent2});
+    @media screen and (min-width: ${({ theme }) => theme.breakpoints.lg}){
+      video{
+        border:0;
+        border-radius:${({ theme }) => theme.border.outer};
+      }
 
-  z-index:10;
-  position:absolute;
-  left:50%;
-  top:50%;
-  transform:translate(-50%, -50%);
-  font-weight:bold;
-  font-size:3em;
-  width:100%;
-  // opacity:1;
-
-}
-@media screen and (min-width: ${({ theme }) => theme.breakpoints.lg}){
-video{
-
-  border:.25em solid ${({ theme }) => theme.color.accent};;
-  border-radius:${({ theme }) => theme.border.outer}
- 
-
-}
-div{
-  border-radius:${({ theme }) => theme.border.outer}
-}
-}
+      div{
+        border-radius:${({ theme }) => theme.border.outer}
+      }
+    }
 `
 
 
@@ -76,7 +65,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeroContainer>
-        <div></div>
+        <div>
+
+        </div>
         <video autoPlay loop muted >
           <source src={Hero} type="video/mp4" />
         </video>
